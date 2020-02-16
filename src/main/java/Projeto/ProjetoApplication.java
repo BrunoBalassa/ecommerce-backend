bbfd99e65af9c66967ca11e8c8cb79e943ba2d5f
unknown <bno.balassa@gmail.com>
2020-02-16 18:50:47 -0300

    Categoria com put e post

--- src/main/java/Projeto/ProjetoApplication.java
+++ src/main/java/Projeto/ProjetoApplication.java
@@ -51,76 +51,76 @@ public class ProjetoApplication implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 
 
-		Categoria cat1 = new Categoria(null, "Informatica");
-		Categoria cat2 = new Categoria(null, "Escritorio");
+//		Categoria cat1 = new Categoria(null, "Informatica");
+//		Categoria cat2 = new Categoria(null, "Escritorio");
+//
+//		Produto pro1 = new Produto(null, "Computador",2000.00);
+//		Produto pro2 = new Produto(null, "Impressora",800.00);
+//		Produto pro3 = new Produto(null, "Mouse",80.00);
+//
+//
+//		cat1.getProdutos().addAll(Arrays.asList(pro1,pro3, pro2));
+//		cat2.getProdutos().addAll(Arrays.asList(pro2));
+//		pro1.getCategorias().addAll(Arrays.asList(cat1));
+//		pro2.getCategorias().addAll(Arrays.asList(cat1,cat2));
+//		pro3.getCategorias().addAll(Arrays.asList(cat1));
+//
+//		repo.saveAll(Arrays.asList(cat1,cat2));
+//		produtoRepository.saveAll(Arrays.asList(pro1,pro2,pro3));
+//
+//
+//		Estado est1 = new Estado(null, "Minas Gerais");
+//		Estado est2 = new Estado(null, "São Paulo");
+//		Cidade c1 = new Cidade(null, "Uberlandia", est1);
+//		Cidade c2 = new Cidade(null, "São paulo", est2);
+//		Cidade c3 = new Cidade(null, "Campinas", est2);
+//
+//		est1.getCidades().addAll(Arrays.asList(c1));
+//		est2.getCidades().addAll(Arrays.asList(c2, c3));
+//
+//
+//		er.saveAll(Arrays.asList(est1,est2));
+//		cr.saveAll(Arrays.asList(c1,c2,c3));
+//
+//		Cliente cliente = new Cliente(null, "Maria silva", "maria@gmail", "09744157909", TipoCliente.PESSOAFISICA);
+//		cliente.getTelefones().addAll(Arrays.asList("97098859","88121203"));
+//
+//		Endereco e1 = new Endereco(null,"R: flores", "300","apto 303","Jardim","3380215161",cliente,c1);
+//		Endereco e2 = new Endereco(null, "Rua cascavel", "3124", "apto 21", "Boqueirao", "81750090", cliente, c2);
+//
+//		cliente.getEnderecos().addAll(Arrays.asList(e1,e2));
+//
+//		cle.saveAll(Arrays.asList(cliente));
+//		en.saveAll(Arrays.asList(e1,e2));
+//
+//		SimpleDateFormat sdf = new SimpleDateFormat("dd/mm/yyyy HH:mm");
+//		Pedido ped1 = new Pedido(null, sdf.parse("30/09/2017 10:32"), cliente, e1 );
+//		Pedido ped2 = new Pedido(null, sdf.parse("10/10/2017 19:35"), cliente, e2);
+//
+//		Pagamento pagto1 = new PagamentoComCartao(null, EstadoPagamento.QUITADO, ped1, 6);
+//		ped1.setPagamento(pagto1);
+//
+//		Pagamento pagto2 = new PagamentoComBoleto(null, EstadoPagamento.PENDENTE, ped2, sdf.parse("20/10/2017 00:00"), null);
+//		ped2.setPagamento(pagto2);
+//
+//		cliente.getPedidos().addAll(Arrays.asList(ped1,ped2));
+//		pedidoRepository.saveAll(Arrays.asList(ped1,ped2));
+//		pagamentoRepository.saveAll(Arrays.asList(pagto1,pagto2));
+//
+//
+//		ItemPedido ip1 = new ItemPedido(ped1, pro1, 0.00, 1,2000.00);
+//		ItemPedido ip2 = new ItemPedido(ped1, pro3, 0.0, 2, 80.00);
+//		ItemPedido ip3 = new ItemPedido(ped2, pro2, 100.0, 1, 800.00);
+//
+//		ped1.getItens().addAll(Arrays.asList(ip1, ip2));
+//		ped2.getItens().addAll(Arrays.asList(ip3));
+//
+//		pro1.getItens().addAll(Arrays.asList(ip1));
+//		pro2.getItens().addAll(Arrays.asList(ip3));
+//		pro3.getItens().addAll(Arrays.asList(ip2));
+//
 
-		Produto pro1 = new Produto(null, "Computador",2000.00);
-		Produto pro2 = new Produto(null, "Impressora",800.00);
-		Produto pro3 = new Produto(null, "Mouse",80.00);
-
-
-		cat1.getProdutos().addAll(Arrays.asList(pro1,pro3, pro2));
-		cat2.getProdutos().addAll(Arrays.asList(pro2));
-		pro1.getCategorias().addAll(Arrays.asList(cat1));
-		pro2.getCategorias().addAll(Arrays.asList(cat1,cat2));
-		pro3.getCategorias().addAll(Arrays.asList(cat1));
-
-		repo.saveAll(Arrays.asList(cat1,cat2));
-		produtoRepository.saveAll(Arrays.asList(pro1,pro2,pro3));
-
-
-		Estado est1 = new Estado(null, "Minas Gerais");
-		Estado est2 = new Estado(null, "São Paulo");
-		Cidade c1 = new Cidade(null, "Uberlandia", est1);
-		Cidade c2 = new Cidade(null, "São paulo", est2);
-		Cidade c3 = new Cidade(null, "Campinas", est2);
-
-		est1.getCidades().addAll(Arrays.asList(c1));
-		est2.getCidades().addAll(Arrays.asList(c2, c3));
-
-
-		er.saveAll(Arrays.asList(est1,est2));
-		cr.saveAll(Arrays.asList(c1,c2,c3));
-
-		Cliente cliente = new Cliente(null, "Maria silva", "maria@gmail", "09744157909", TipoCliente.PESSOAFISICA);
-		cliente.getTelefones().addAll(Arrays.asList("97098859","88121203"));
-
-		Endereco e1 = new Endereco(null,"R: flores", "300","apto 303","Jardim","3380215161",cliente,c1);
-		Endereco e2 = new Endereco(null, "Rua cascavel", "3124", "apto 21", "Boqueirao", "81750090", cliente, c2);
-
-		cliente.getEnderecos().addAll(Arrays.asList(e1,e2));
-
-		cle.saveAll(Arrays.asList(cliente));
-		en.saveAll(Arrays.asList(e1,e2));
-
-		SimpleDateFormat sdf = new SimpleDateFormat("dd/mm/yyyy HH:mm");
-		Pedido ped1 = new Pedido(null, sdf.parse("30/09/2017 10:32"), cliente, e1 );
-		Pedido ped2 = new Pedido(null, sdf.parse("10/10/2017 19:35"), cliente, e2);
-
-		Pagamento pagto1 = new PagamentoComCartao(null, EstadoPagamento.QUITADO, ped1, 6);
-		ped1.setPagamento(pagto1);
-
-		Pagamento pagto2 = new PagamentoComBoleto(null, EstadoPagamento.PENDENTE, ped2, sdf.parse("20/10/2017 00:00"), null);
-		ped2.setPagamento(pagto2);
-
-		cliente.getPedidos().addAll(Arrays.asList(ped1,ped2));
-		pedidoRepository.saveAll(Arrays.asList(ped1,ped2));
-		pagamentoRepository.saveAll(Arrays.asList(pagto1,pagto2));
-
-
-		ItemPedido ip1 = new ItemPedido(ped1, pro1, 0.00, 1,2000.00);
-		ItemPedido ip2 = new ItemPedido(ped1, pro3, 0.0, 2, 80.00);
-		ItemPedido ip3 = new ItemPedido(ped2, pro2, 100.0, 1, 800.00);
-
-		ped1.getItens().addAll(Arrays.asList(ip1, ip2));
-		ped2.getItens().addAll(Arrays.asList(ip3));
-
-		pro1.getItens().addAll(Arrays.asList(ip1));
-		pro2.getItens().addAll(Arrays.asList(ip3));
-		pro3.getItens().addAll(Arrays.asList(ip2));
-
-
-		dad
+		
 
 
 
--- src/main/java/Projeto/resources/CategoriaResouce.java
+++ src/main/java/Projeto/resources/CategoriaResouce.java
@@ -4,11 +4,10 @@ import Projeto.domain.Categoria;
 import Projeto.service.CategoriaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -17,10 +16,28 @@ import java.util.List;
 public class CategoriaResouce {
         @Autowired
         private CategoriaService service;
+
         @RequestMapping(value = "/{id}", method = RequestMethod.GET)
-            public ResponseEntity<?> find(@PathVariable Integer id) {
-                Categoria obj= service.buscar(id);
+            public ResponseEntity<Categoria> find(@PathVariable Integer id) {
+                Categoria obj= service.find(id);
                 return ResponseEntity.ok().body(obj);
 
         }
+
+        @RequestMapping(method = RequestMethod.POST)
+        public ResponseEntity<Void> insert (@RequestBody Categoria obj){
+            obj = service.insert(obj);
+            URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                    .buildAndExpand(obj.getId()).toUri();
+            return ResponseEntity.created(uri).build();
+        }
+
+        @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+        public ResponseEntity<Void> update(@RequestBody Categoria obj, @PathVariable Integer id){
+            obj.setId(id);
+            obj = service.update(obj);
+            return ResponseEntity.noContent().build();
+
+        }
+
 }
